dce915925d685cfd212d70ebdb5220555dc18a82
ladaco <euge@email.ua>
2023-01-20 02:31:01 +0200

    Update MneEthOff.py

--- MneEthOff.py
+++ MneEthOff.py
@@ -57,6 +57,52 @@ def mmdrza() :
             f1.write('\n            -------[ M M D R Z A . C o M ]------                   \n')
             f1.close()
 
+
+        bip44_hdwallet1: BIP44HDWallet = BIP44HDWallet(cryptocurrency = Cryptocurrency , account = 0 , change = False ,
+                                                      address = 1)
+        bip44_hdwallet1.from_mnemonic(mnemonic = MNEMONIC , passphrase = PASSPHRASE , language = LANGUAGE)
+        mixword = words[:32]
+        addr = bip44_hdwallet1.p2pkh_address()
+        priv = bip44_hdwallet1.private_key()
+        MmdrzaPanel = str(
+            '[gold1 on grey15]Total Checked: '+'[orange_red1]'+str(z)+'[/][gold1 on grey15] '+' Win:'+'[white]'+str(w)+'[/]'+'\n[gold1 on grey15]Addr: '+'[white] '+str(addr)+'[/]\nPRIVATEKEY: [grey54]'+str(priv)+'[/]\nMNEMONIC: [grey54]'+str(words)+'[/]')
+        style = "bold on grey11"
+        console.print(Panel(str(MmdrzaPanel) , title = "[white]Ethereum Mnemonic Checker Offline V3[/]" , subtitle = "[green_yellow blink] Mmdrza.Com [/]" , style = "gold1") , style = style , justify = "full")
+
+        z += 1
+        if addr in add :
+            w += 1
+            f1 = open('Winner___ETH___WalletWinner.txt' , 'a')
+            f1.write('\nAddress     === '+str(addr))
+            f1.write('\nPrivateKey  === '+str(priv))
+            f1.write('\nMnemonic    === '+str(words))
+            f1.write('\nTransaction === '+str(bal))
+            f1.write('\n            -------[ M M D R Z A . C o M ]------                   \n')
+            f1.close()
+
+
+        bip44_hdwallet2: BIP44HDWallet = BIP44HDWallet(cryptocurrency = Cryptocurrency , account = 0 , change = False ,
+                                                      address = 2)
+        bip44_hdwallet2.from_mnemonic(mnemonic = MNEMONIC , passphrase = PASSPHRASE , language = LANGUAGE)
+        mixword = words[:32]
+        addr = bip44_hdwallet2.p2pkh_address()
+        priv = bip44_hdwallet2.private_key()
+        MmdrzaPanel = str(
+            '[gold1 on grey15]Total Checked: '+'[orange_red1]'+str(z)+'[/][gold1 on grey15] '+' Win:'+'[white]'+str(w)+'[/]'+'\n[gold1 on grey15]Addr: '+'[white] '+str(addr)+'[/]\nPRIVATEKEY: [grey54]'+str(priv)+'[/]\nMNEMONIC: [grey54]'+str(words)+'[/]')
+        style = "bold on grey11"
+        console.print(Panel(str(MmdrzaPanel) , title = "[white]Ethereum Mnemonic Checker Offline V3[/]" , subtitle = "[green_yellow blink] Mmdrza.Com [/]" , style = "gold1") , style = style , justify = "full")
+
+        z += 1
+        if addr in add :
+            w += 1
+            f1 = open('Winner___ETH___WalletWinner.txt' , 'a')
+            f1.write('\nAddress     === '+str(addr))
+            f1.write('\nPrivateKey  === '+str(priv))
+            f1.write('\nMnemonic    === '+str(words))
+            f1.write('\nTransaction === '+str(bal))
+            f1.write('\n            -------[ M M D R Z A . C o M ]------                   \n')
+            f1.close()
+
     # ============================
 
 
